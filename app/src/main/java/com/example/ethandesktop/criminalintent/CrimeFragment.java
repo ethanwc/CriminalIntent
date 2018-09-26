b832a82cae1eaa8843bdb65b0ccb61a78bd1bcce
ethanwc <ethanwcheatham@gmail.com>
2018-09-25 23:31:25 -0700

    Menu and stuff

--- app/src/main/java/com/example/ethandesktop/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/ethandesktop/criminalintent/CrimeFragment.java
@@ -1,7 +1,10 @@
 package com.example.ethandesktop.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -12,14 +15,17 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
 
-    private static final String ARG_CRIME_ID = "crime_id";
+    private static final String ARG_CRIME_ID = "crime_id", DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
     private EditText mTitleField;
-    private Button mDateBUtton;
+    private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
     @Override
@@ -61,14 +67,36 @@ public class CrimeFragment extends Fragment {
             }
         });
 
-        mDateBUtton = (Button) v.findViewById(R.id.crime_date);
-        mDateBUtton.setText(mCrime.getDate().toString());
-        mDateBUtton.setEnabled(false);
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        updateDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
                 return v;
         }
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode != Activity.RESULT_OK) return;
 
-        public static CrimeFragment newInstance(UUID crimeId) {
+            if (requestCode == REQUEST_DATE) {
+                Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+                mCrime.setDate(date);
+                updateDate();
+            }
+        }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+
+    public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
         args.putSerializable(ARG_CRIME_ID, crimeId);
 
@@ -76,4 +104,5 @@ public class CrimeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
         }
+
     }
